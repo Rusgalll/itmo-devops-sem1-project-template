8614e18acd83384b47fbe8fdbebfcd2645a54774
Ruslan <ruslan.gali.galimullin@yandex.ru>
2025-02-26 22:44:27 +0300

    Добавлены скрипты для инициализации

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+set -e
+
+# Загрузка переменных из переменного окржуения для инициализации
+if [ -f .env ]; then
+    export $(cat -v '^#' .env | xargs)
+else
+    echo "Ошибка: не найден .env файл"
+    exit 1
+fi
+
+#PostgreSQL
+if ! command -v psql &> /dev/null
+then
+    echo "Ошибка: необходимо установить PostgreSQL"
+    exit 1
+fi
+
+echo "Создание таблицы $DB_TABLE_NAMEE в базе данных $DB_NAME..."
+
+PGPASSWORD=$DB_PASSWORD psql -U $DB_USER_NAME -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "
+CREATE TABLE IF NOT EXISTS $DB_TABLE_NAME (
+    id SERIAL PRIMARY KEY,           -- Автоматически увеличиваемый идентификатор
+    created_at DATE NOT NULL,        -- Дата создания продукта
+    name VARCHAR(255) NOT NULL,      -- Название продукта
+    category VARCHAR(255) NOT NULL,  -- Категория продукта
+    price DECIMAL(10, 2) NOT NULL    -- Цена продукта с точностью до 2 знаков после запятой
+);"
+
+echo "База данных инициалзирована успешно"
+
+#Go
+echo "Устанавка Go зависимостей..."
+go mod tidy
+echo "Go Зависимости установлены"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+set -e
+
+if lsof -i:8080 -t >/dev/null 2>&1; then  
+  PID=$(lsof -i:8080 -t)
+  echo "Обнаружен процесс на порту 8080 с PID: $PID"  
+  kill -9 "$PID"
+  echo "Процесс успешно завершён"
+fi
+
+echo "Сборка приложения..."
+
+if ! command -v go >/dev/null 2>&1; then  
+  echo "Ошибка: Go не установлен"
+  exit 1
+fi
+
+if go build -o app ./main.go; then
+  echo "Сборка завершена успешно"
+else
+  echo "Ошибка: не удалась сборка приложения" 
+  exit 1
+fi
